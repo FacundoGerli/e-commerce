fc2beba18f1664a8bc0cf46bff3fed7990b586c0
Facundo Gerli <Facundogerli@hotmail.com>
2025-03-02 00:52:40 -0300

    testing

--- api-gateway/src/main/java/config/SecurityConfiguration.java
+++ api-gateway/src/main/java/config/SecurityConfiguration.java
@@ -15,15 +15,24 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
-;
+;import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.anyExchange;
+import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers;
 
 @EnableWebFluxSecurity
 @Configuration
 public class SecurityConfiguration {
+
+    @Value("${spring.security.keycloakJwksUrl}")
+    private String keycloakJwksUrl;
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
         httpSecurity
                 .oauth2Login(Customizer.withDefaults())
+                .authorizeExchange(exchange -> exchange
+                    .pathMatchers("/public/**").permitAll()
+                        .anyExchange().authenticated()
+                )
                 .addFilterAfter(jwtUserIdFilter(), SecurityWebFiltersOrder.AUTHORIZATION)
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return httpSecurity.build();
@@ -53,8 +62,6 @@ public class SecurityConfiguration {
             return null;
         }
     }
-    @Value("${spring.security.keycloakJwksUrl}")
-    private String keycloakJwksUrl;
     @Bean
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withJwkSetUri(keycloakJwksUrl).build();
